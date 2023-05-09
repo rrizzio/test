0e852fa22232501e62f941a9dc3c42970fa9b7de
rrizzio <rogerio.rizzio@gmail.com>
2023-05-09 11:18:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("Testing Streamlit")
+streamlit.title("My Healthy Diner")
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🍲 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥦 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥚 Hard-Boiled Free-Range Egg')
+
