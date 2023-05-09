e78d4ba9e527fb79b2747ad3098d2d0d62ec4797
rrizzio <rogerio.rizzio@gmail.com>
2023-05-09 11:33:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas
 
 streamlit.title("My Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🍲 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥦 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥚 Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+streamlit.dataframe(my_fruit_list)
+
 
