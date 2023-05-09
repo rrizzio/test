abd2e3dc8372b57cf8ed586c7d163ec60a04eecb
rrizzio <rogerio.rizzio@gmail.com>
2023-05-09 18:21:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Healthy Diner")
 streamlit.header('Breakfast Menu')
@@ -16,6 +17,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+# Calling an API
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/apple")
+streamlit.text(fruityvice_response)
 
 
 
