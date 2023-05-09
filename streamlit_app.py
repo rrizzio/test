1d4e3798953850bff9aa66425e898b0168103465
rrizzio <rogerio.rizzio@gmail.com>
2023-05-09 11:35:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text(' 🍲 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥦 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥚 Hard-Boiled Free-Range Egg')
 
-my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
 
